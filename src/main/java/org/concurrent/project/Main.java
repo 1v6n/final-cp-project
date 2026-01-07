e9b979cc11c59a0e40563cf751d6598ec80cac11
AguTrachta <agustrachta@gmail.com>
2026-01-07 18:20:17 -0300

    refactor: inicializar logger y ejecutar hilos dentro de try-with-resources

--- src/main/java/org/concurrent/project/Main.java
+++ src/main/java/org/concurrent/project/Main.java
@@ -1,5 +1,8 @@
 package org.concurrent.project;
 
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Vector;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -8,45 +11,51 @@ public class Main {
 
     public static void main(String[] args) {
         RdP rdP = new RdP();
-        Monitor monitor = new Monitor(rdP);
 
-        int[][] invariants = {
-                {1, 2, 5, 6, 9, 10, 11},
-                {1, 3, 4, 6, 9, 10, 11},
-                {1, 2, 5, 7, 8, 11},
-                {1, 3, 4, 7, 8, 11}
-        };
+        String stamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
+        Path logPath = Path.of("logs", "run_" + stamp + ".log");
 
-        AtomicInteger invariantCounter = new AtomicInteger(TOTAL_RUNS);
-        Vector<Integer> pathForThread0 = new Vector<>();
-        pathForThread0.add(0);
-        Thread thread0 = new Thread(new Threads(pathForThread0, TOTAL_RUNS, rdP, monitor, invariantCounter, true), "Thread-0");
+        try (LogService logger = new LogService(logPath)) {
+            Monitor monitor = new Monitor(rdP, logger);
 
-        Thread[] invariantThreads = new Thread[invariants.length];
-        for (int i = 0; i < invariants.length; i++) {
-            Vector<Integer> pathForInvariant = new Vector<>();
-            for (int transition : invariants[i]) {
-                pathForInvariant.add(transition);
+            int[][] invariants = {
+                    {1, 2, 5, 6, 9, 10, 11},
+                    {1, 3, 4, 6, 9, 10, 11},
+                    {1, 2, 5, 7, 8, 11},
+                    {1, 3, 4, 7, 8, 11}
+            };
+
+            AtomicInteger invariantCounter = new AtomicInteger(TOTAL_RUNS);
+
+            Vector<Integer> pathForThread0 = new Vector<>();
+            pathForThread0.add(0);
+            Thread thread0 = new Thread(
+                    new Threads(pathForThread0, TOTAL_RUNS, rdP, monitor, invariantCounter, true),
+                    "Thread-0"
+            );
+
+            Thread[] invariantThreads = new Thread[invariants.length];
+            for (int i = 0; i < invariants.length; i++) {
+                Vector<Integer> pathForInvariant = new Vector<>();
+                for (int transition : invariants[i]) pathForInvariant.add(transition);
+
+                invariantThreads[i] = new Thread(
+                        new Threads(pathForInvariant, 0, rdP, monitor, invariantCounter, false),
+                        "Invariant-Thread-" + (i + 1)
+                );
             }
-            invariantThreads[i] = new Thread(new Threads(pathForInvariant, 0, rdP, monitor, invariantCounter, false),
-                    "Invariant-Thread-" + (i + 1));
-        }
 
-        thread0.start();
-        for (Thread thread : invariantThreads) {
-            thread.start();
-        }
+            thread0.start();
+            for (Thread thread : invariantThreads) thread.start();
 
-        try {
             thread0.join();
-            for (Thread thread : invariantThreads) {
-                thread.join();
-            }
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
+            for (Thread thread : invariantThreads) thread.join();
+
+            System.out.println("All threads have completed.");
+            System.out.println("Log written to: " + logPath.toAbsolutePath());
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
-        System.out.println("All threads have completed.");
     }
 }
